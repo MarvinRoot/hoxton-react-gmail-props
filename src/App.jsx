fc6205cd9fb6b31d407605b3474a8d19ccd06c01
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-01-06 19:34:45 +0100

    Finished dividing in components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import initialEmails from './data/emails'
 import './App.css'
 import Header from './components/Header'
 import LeftMenu from './components/LeftMenu/LeftMenu'
+import EmailsList from './components/Emails/EmailsList'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
 
@@ -56,35 +57,10 @@ function App() {
       setHideRead = {setHideRead}
       />
 
-      <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <EmailsList filteredEmail = {filteredEmails}
+       toggleRead = {toggleRead} 
+       toggleStar = {toggleStar} />
+       
     </div>
   )
 }
